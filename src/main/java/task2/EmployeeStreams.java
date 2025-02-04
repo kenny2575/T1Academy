5770be6f916587b32fd432bd0d9af0719dc2bf96
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-02-04 20:07:29 +0700

    Реализация Stream

---
+++ src/main/java/task2/EmployeeStreams.java
@@ -0,0 +1,22 @@
+package task2;
+
+import java.util.List;
+
+public class EmployeeStreams {
+
+    public List<Employee> getThreeEldestEngineers(List<Employee> list) {
+        return list.stream()
+                .filter(employee -> employee.jobTitle().equals("Engineer"))
+                .sorted((o1,o2)-> o2.age() - o1.age())
+                .limit(3)
+                .toList();
+    }
+
+    public Double getAverageAge(List<Employee> list) {
+        return list.stream()
+                .filter(employee -> employee.jobTitle().equals("Engineer"))
+                .mapToInt(Employee::age)
+                .average()
+                .orElseThrow();
+    }
+}
