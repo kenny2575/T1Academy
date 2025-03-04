e2539a9f9cb58702f4339302ced817afded2a958
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-03-04 16:45:27 +0700

    Класс использующий аннотации

--- src/main/java/org/example/task1/ObservedClass.java
+++ src/main/java/org/example/task1/ObservedClass.java
@@ -1,6 +1,8 @@
 package org.example.task1;
 
-import org.example.task1.annotation.BeforeSuite;
+import org.example.task1.annotation.*;
+
+import java.sql.SQLOutput;
 
 public class ObservedClass {
 
@@ -8,4 +10,45 @@ public class ObservedClass {
     void callBeforeSuite() {
         System.out.println("before suite method");
     }
+
+    @AfterSuite
+    void callAfterSuite() {
+        System.out.println("after suite method");
+    }
+
+    @BeforeTest
+    void beforeTest() {
+        System.out.println("Before test");
+    }
+
+    @AfterTest
+    void afterTest(){
+        System.out.println("After test");
+    }
+
+    @Test(2)
+    void testPriorityTwo(){
+        System.out.println("Test priority 2");
+    }
+
+    @Test
+    void testDefaultPriority() {
+        System.out.println("Default priority");
+    }
+
+    @Test(5)
+    void testPriorityFive() {
+        System.out.println("Test priority 5");
+    }
+
+    @Test(4)
+    @CsvSource("10,Test,40,false")
+    void testPriorityFour(int a, String b, int c, boolean d){
+        System.out.println("Test priority 4 with parsed: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+
 }
